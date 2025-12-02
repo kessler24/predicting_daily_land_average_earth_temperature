d4ab18c1c57ecf3eddf7342cbfcc8cd30e7d8551
Jacob Cann <jfc25@student.ubc.ca>
2025-12-02 15:39:43 -0800

    create abstract_eda.py script

---
+++ scripts/abstract_eda.py
@@ -0,0 +1,36 @@
+# documentation comments
+"""
+
+
+
+
+"""
+
+# import libraries/packages
+#--------------------
+
+import pandas as pd
+import altair as alt
+
+# parse/define command line arguments here
+#--------------------
+
+
+# code for other functions
+#--------------------
+
+def viz_tabular_stats(df):
+    ...
+
+# define main function
+#--------------------
+
+def main():
+    # code for "guts" of script goes here
+
+    
+# call main function
+#--------------------
+
+if __name__ == "__main__":
+    main() # pass any command line args to main here
