0070b95f8008819de2d640c30accb781cd999487
daisyzhou-ubc <daisyzhou25ca@gmail.com>
2025-12-06 22:29:17 -0800

    Update data_preprocessing.py
    Update command line to staring from the root

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -27,11 +27,11 @@ Usage
 -----
 Run this script from the command line:
 
-    python data_preprocessing.py \
-        --read_path="../data/global_temp_anomaly_raw.csv" \
-        --write_path="../data/global_temp_anomaly_cleaned" \
-        --plots_path="../images" \
-        --logs_path="../logs"
+    python scripts/data_preprocessing.py \
+        --read_path="data/global_temp_anomaly_raw.csv" \
+        --write_path="data/global_temp_anomaly_cleaned" \
+        --plots_path="images" \
+        --logs_path="logs"
 
 Outputs
 -------
