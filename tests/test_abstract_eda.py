eee8f5fb210f6bed39dffed9c0526703046ab0b3
Jacob Cann <jfc25@student.ubc.ca>
2025-12-08 22:29:12 -0800

    two test functions written

--- tests/test_abstract_eda.py
+++ tests/test_abstract_eda.py
@@ -10,25 +10,34 @@ from scripts.abstract_eda import (increment_filename,
                                   viz_seasonal_lines,
                                   viz_density_dists)
 
-
 """
 Test increment_filename from abstract_eda.py script
 """
-
 def test_increment_filename():
-    assert increment_filename('images/eda_1') == 'images/eda_2'
-
+    # Return type check
+    assert isinstance(increment_filename('images/eda_1.png'), str)
+    # Typical example check
+    assert increment_filename('images/eda_1.png') == 'images/eda_2.png'
+    # Atypical example check
+    assert increment_filename('images/eda_1_1.png') == 'images/eda_1_2.png'
 
 """
 Test add_prefix_to_increment from abstract_eda.py script
 """
-
+def test_add_prefix_to_increment():
+    # Return type check
+    assert isinstance(add_prefix_to_increment('images/eda_1.png', 'plot'), str)
+    # Typical example check
+    assert add_prefix_to_increment('images/eda_1.png','plot') == 'images/eda_plot_1.png'
+    # Atypical example check
+    assert add_prefix_to_increment('images/train_df_1.csv', 'table') == 'images/train_df_table_1.csv'
 
 
 """
 Test read_clean_data from abstract_eda.py script
 """
-
+def test_read_clean_data():
+    assert read_clean_data('')
 
 
 """
