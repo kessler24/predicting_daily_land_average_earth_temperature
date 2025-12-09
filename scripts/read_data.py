a4d76e1d7bb8e6b58ce054c65f6b302c9807eeae
daisyzhou-ubc <daisyzhou25ca@gmail.com>
2025-12-09 13:31:18 -0800

    add function to read_data.py

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -38,13 +38,8 @@ is whitespace-delimited. These are automatically handled using pandas'
 read_csv options.
 """
 
-import click
-import pandas as pd
+def download_temperature_data(input_path: str, output_path: str):
 
-@click.command()
-@click.argument("input_path")
-@click.argument("output_path")
-def main(input_path, output_path):
     """
     Load raw Berkeley Earth daily temperature anomaly data and save as CSV.
 
@@ -79,23 +74,48 @@ def main(input_path, output_path):
 
     These are applied automatically after reading the file.
     """
-    
+
     # make sure the input_path (first argument is a proper url before downloading)
     if input_path.startswith("http://") or input_path.startswith("https://"):
         print(f"Downloading data from an URL: {input_path} ")
     else:
         print(f"Reading local file from {input_path}")
-
-    # read the data with pandas to a csv 
-    df = pd.read_csv(input_path, sep=r"\s+", comment="%", header=None)
-    column_names = ["Date Number", "Year", "Month", "Day" , "Day of Year", "Anomaly"]
+    
+    # Make sure the file can be read as a csv file format
+    try:
+        df = pd.read_csv(input_path, sep=r"\s+", comment="%", header=None)
+    except Exception as e:
+        print(f"Error reading data with the file path: {input_path}: {e}")
+        # Re-raise as IOError for proper function testing/robustness
+        raise IOError(f"The input file path: {input_path} is not readable/downloadable") from e
+    
+    # rename data columns 
+    column_names = ["Date Number", "Year", "Month", "Day", "Day of Year", "Anomaly"]
     df.columns = column_names
-
+    
+    # write and save the data as a csv file
     if output_path.endswith(".csv"):
         df.to_csv(output_path, index=False)
         print(f"Data saved to {output_path}")
     else:
         print("Error: Please be aware that your data has not been written as the output_path is not to a .csv file.")
+
+
+@click.command()
+@click.argument("input_path")
+@click.argument("output_path")
+def main(input_path, output_path):
+    """
+    Download raw Berkeley Earth daily temperature anomaly data and save as a csv file.
+    """
+    # calls the processing function and handles exceptions for the function
+    try:
+        download_temperature_data(input_path, output_path)
+    except IOError as e:
+        print(f"FATAL ERROR: Processing failed. {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 
 if __name__ == "__main__":
     main()
